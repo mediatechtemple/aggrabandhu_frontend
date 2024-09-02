d1a782910492b0cdf3e03242a199e4af9144dba4
AshokaYadav <yashoka5111718258@gmail.com>
2024-09-02 13:03:52 +0530

    profile cahnge 3rd

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h2>Mona is working on deployement...and other stuff...</h2>
+      <h2>Hell mona........</h2>
     </div>
   )
 }
