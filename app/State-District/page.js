8023eb0c95c32bca96c26d4eadbf7ca084807147
mediatechtemple <83817361+mediatechtemple@users.noreply.github.com>
2024-09-25 17:58:40 +0530

    Update page.js

--- app/State-District/page.js
+++ app/State-District/page.js
@@ -1,6 +1,7 @@
 import StateDistrictManager from '@/components/Input/State_DIstrict/State_DIstict'
 import React from 'react'
 // import StateDitrictManager from '@/component/Input/State_DIstict'
+//all the user signup
 const page = () => {
   return (
     <div>
